8bfd7169ccd5f998d29ce193d2a5bc9a9d7b5005
ambati raghu <ambati.raghu@karvy.com>
2020-02-24 09:51:09 +0530

    jquery install

--- src/app/home/sender/sender.component.ts
+++ src/app/home/sender/sender.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import './create'
 import './subscribing'
 import './asynchSubject'
 import { DataService } from './data.service';
+
+declare var $:any;
 
 @Component({
   selector: 'app-sender',
@@ -11,16 +13,42 @@ import { DataService } from './data.service';
   styleUrls: ['./sender.component.css']
 })
 export class SenderComponent implements OnInit {
+  htmlSnippet = 'Template <script>alert("XSS Attack")</script> <b>Code attached</b>';
 
   message:string;
-  constructor(private dataService:DataService) { }
+  safeOperator = null;
+  validation = {"name":"test",
+                  "adress":"",
+                  "status":true};
+  greeting: string[] = ['a','b', 'c', 'd', 'f', 'g', 'i'];
+  @ViewChild('myname', {static: false}) input ; 
+  constructor(private dataService:DataService,private MyElement:ElementRef) {
+    
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    $(document).ready(()=> {
+      
+      $('#elementId').css({'text-color':'blue','font-size':'150%'});
+    });
 
     this.dataService.currentMessage.subscribe(message => this.message=message)
   }
 
   changeName(mesage:string) {
     this.dataService.newMessage(mesage);
+  }
+
+  myName:string = "Raghu";
+  updateName(name: string) {
+    this.myName = name;
+  }
+
+
+  @ViewChild('uname', {static: true}) uname;
+
+  ngAfterViewInit() {
+    console.log("------"+this.uname.nativeElement.value);
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginRouting } from './login.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { LoginRouting } from './login.routing';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    LoginRouting
+    LoginRouting,
+    HttpClientModule
   ]
 })
 export class LoginModule { }
